6506ee1f75f34f307dca74e79f814c28b66292b7
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-16 01:02:32 +0900

    :recycle: 動的グループ & IAMポリシー追加

---
+++ envs/iam.tf
@@ -0,0 +1,29 @@
+/************************************************************
+Dynamic Group - Compute
+************************************************************/
+# 「oci_identity_dynamic_group」を使用する場合はルートコンパートメントのDefaultアイデンティティドメインにしか作成できない
+# 「oci_identity_domains_dynamic_resource_group」を使用すれば、指定のアイデンティティドメインに作成可能
+resource "oci_identity_dynamic_group" "compute" {
+  compartment_id = var.tenancy_ocid
+  name           = "Compute_Dynamic_Group"
+  description    = "Compute Dynamic Group"
+  matching_rule = format(
+    "All {instance.compartment.id = '%s', tag.Compute.CloudAgent.value = 'true'}",
+    oci_identity_compartment.workload.id
+  )
+  defined_tags = local.common_defined_tags
+}
+
+/************************************************************
+IAM Policy - Compute
+************************************************************/
+resource "oci_identity_policy" "compute_for_agent_logmoni" {
+  compartment_id = oci_identity_compartment.workload.id
+  description    = "OCI Compute Policy for CloudAgent Custom Log Monitoring"
+  name           = "compute-cloudagent-log-monitoring-policy"
+  statements = [
+    format("allow dynamic-group %s to use log-content in compartment %s", oci_identity_dynamic_group.compute.name, oci_identity_compartment.workload.name)
+  ]
+  defined_tags = local.common_defined_tags
+}
+
