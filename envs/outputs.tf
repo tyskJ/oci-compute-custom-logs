430b97a497bf68dccaad34f55a9197ff0a14338e
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-16 21:54:18 +0900

    :memo: sshセッション後のsshコマンド追加

--- envs/outputs.tf
+++ envs/outputs.tf
@@ -23,3 +23,10 @@ output "ssh_port_forwarding_oracle" {
     -p 22 ${oci_bastion_session.ssh_port_forwarding_oracle.id}@host.bastion.${local.region_map["NRT"]}.oci.oraclecloud.com
   EOT
 }
+
+output "ssh_command_after_ssh_port_forwarding_oracle" {
+  value = <<-EOT
+    ssh -i ./.key/private_oracle.pem \
+    -p 22222 opc@localhost
+  EOT
+}
