d74e116f4ec0c60f09d021e62d3b1c0e329470f6
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-17 00:34:12 +0900

    :memo: エージェント構成にて動的グループ指定が必要なため、OS毎に分ける

--- envs/iam.tf
+++ envs/iam.tf
@@ -3,12 +3,23 @@ Dynamic Group - Compute
 ************************************************************/
 # 「oci_identity_dynamic_group」を使用する場合はルートコンパートメントのDefaultアイデンティティドメインにしか作成できない
 # 「oci_identity_domains_dynamic_resource_group」を使用すれば、指定のアイデンティティドメインに作成可能
-resource "oci_identity_dynamic_group" "compute" {
+resource "oci_identity_dynamic_group" "compute_oracle" {
   compartment_id = var.tenancy_ocid
-  name           = "Compute_Dynamic_Group"
-  description    = "Compute Dynamic Group"
+  name           = "Compute_Dynamic_Group_Oracle"
+  description    = "Compute Dynamic Group Oracle"
   matching_rule = format(
-    "All {instance.compartment.id = '%s', tag.Compute.CloudAgent.value = 'true'}",
+    "All {instance.compartment.id = '%s', tag.Compute.CloudAgent.value = 'oracle'}",
+    oci_identity_compartment.workload.id
+  )
+  defined_tags = local.common_defined_tags
+}
+
+resource "oci_identity_dynamic_group" "compute_windows" {
+  compartment_id = var.tenancy_ocid
+  name           = "Compute_Dynamic_Group_Windows"
+  description    = "Compute Dynamic Group Windows"
+  matching_rule = format(
+    "All {instance.compartment.id = '%s', tag.Compute.CloudAgent.value = 'windows'}",
     oci_identity_compartment.workload.id
   )
   defined_tags = local.common_defined_tags
@@ -22,7 +33,11 @@ resource "oci_identity_policy" "compute_for_agent_logmoni" {
   description    = "OCI Compute Policy for CloudAgent Custom Log Monitoring"
   name           = "compute-cloudagent-log-monitoring-policy"
   statements = [
-    format("allow dynamic-group %s to use log-content in compartment %s", oci_identity_dynamic_group.compute.name, oci_identity_compartment.workload.name)
+    format("allow dynamic-group %s, %s to use log-content in compartment %s",
+      oci_identity_dynamic_group.compute_oracle.name,
+      oci_identity_dynamic_group.compute_windows.name,
+      oci_identity_compartment.workload.name
+    )
   ]
   defined_tags = local.common_defined_tags
 }
