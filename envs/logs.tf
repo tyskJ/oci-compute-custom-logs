9560aa8cab25d1adc3ec566330542dd90d2f3505
tyskJ <193552101+tyskJ@users.noreply.github.com>
2026-03-17 18:54:27 +0900

    :memo: custom logs　追加

--- envs/logs.tf
+++ envs/logs.tf
@@ -12,3 +12,24 @@ resource "oci_logging_log_group" "windows" {
   display_name   = "windows-instance-log-group"
   defined_tags   = local.common_defined_tags
 }
+
+/************************************************************
+Custom Logs - Compute
+************************************************************/
+resource "oci_logging_log" "oracle" {
+  log_group_id       = oci_logging_log_group.oracle.id
+  is_enabled         = true
+  retention_duration = 30
+  display_name       = "nginx"
+  log_type           = "CUSTOM"
+  defined_tags       = local.common_defined_tags
+}
+
+resource "oci_logging_log" "windows" {
+  log_group_id       = oci_logging_log_group.windows.id
+  is_enabled         = true
+  retention_duration = 30
+  display_name       = "nginx"
+  log_type           = "CUSTOM"
+  defined_tags       = local.common_defined_tags
+}
